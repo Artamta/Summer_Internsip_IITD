5cce4cdf1a26292f7d230ef2d54c11790bf06707
Artamta <artamta47@gmail.com>
2025-06-06 15:32:36 +0530

    yoyo

---
+++ python/10_final/final.py
@@ -0,0 +1,41 @@
+import numpy as np
+#from scipy.optimize import curvefit
+import nibabel as nb
+import matplotlib.pyplot as plt
+
+
+#importing files paths:
+original_fmap_Path="/Users/ayush/Desktop/project-internsip/new_work/OneDrive_2_23-05-2025/Simulation-III_Nifty-data/Simulation-III_f_map.nii"
+snr60="/Users/ayush/Desktop/project-internsip/new_work/OneDrive_2_23-05-2025/Simulation-III_Nifty-data/Simulation-III_SNR60/Data-1_Simulation-III_SNR-60.nii"
+
+#output_by_toolbox
+snr60_toolbox_out_D_star="/Users/ayush/Desktop/project-internsip/Output_Parameter_Maps/Data-1_Simulation-III_SNR-60_D_star.nii"
+snr60_toolbox_out_Fmap="/Users/ayush/Desktop/project-internsip/Output_Parameter_Maps/Data-1_Simulation-III_SNR-60_f.nii"
+snr60_toolbox_out_D="/Users/ayush/Desktop/project-internsip/Output_Parameter_Maps/Data-1_Simulation-III_SNR-60_D.nii"
+snr60_toolbox_out_K="/Users/ayush/Desktop/project-internsip/Output_Parameter_Maps/Data-1_Simulation-III_SNR-60_k.nii"
+
+#opening files
+#tool_box
+original_fmap = nb.load(original_fmap_Path)
+snr60_data=nb.load(snr60)
+fmap_toolbox=nb.load(snr60_toolbox_out_Fmap)
+dmap_toolbox=nb.load(snr60_toolbox_out_D)
+d_star_map_toolbox=nb.load(snr60_toolbox_out_D_star)
+k_map_toolbox=nb.load(snr60_toolbox_out_K)
+
+np.arr=original_fmap
+#print(np.arr)
+
+#just 
+
+x=[1,2,3,4]
+y=[1,23,54,653]
+
+plt.plot(x,y,label="linear")
+plt.xlabel=("lun")
+plt.ylabel=("chu")
+plt.title=("sex")
+plt.plot(x,y,label="linear")
+plt.show()
+
+
